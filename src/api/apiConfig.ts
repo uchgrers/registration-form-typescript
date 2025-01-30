e79ed8cc66011e4fd4bc07d54afe9b6aaab437f3
uchgrers <uchgrers@gmail.com>
2025-01-30 13:44:56 +0300

    change base URL

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = process.env.NODE_ENV === "production" ?
-    "https://registration-form-typescript-git-master-uchgrers-projects.vercel.app" : "http://localhost:8000";
+    "https://registration-form-typescript.vercel.app" : "http://localhost:8000";
 
 export const baseRequestParams = axios.create({
     withCredentials: true,
